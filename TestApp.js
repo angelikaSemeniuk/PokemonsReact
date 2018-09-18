4d1b3f95b9936a5cfeec144cb0e9290386705e24
Anzhelika <angelika0094@gmail.com>
2018-09-18 17:11:25 +0300

    Initial commit

---
+++ TestApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class TestApp extends React.Component{
+    render () {
+        return(
+            <h2>Hello World!!!!!!!!</h2>
+        );
+    }
+
+}
